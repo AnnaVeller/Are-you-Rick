be049a92f49c478c6db1139c7b7dd47cda0dc609
AnnaVeller <anna.ovsi@mail.ru>
2021-06-01 23:05:31 +0300

    get one style and reformat code

--- app.js
+++ app.js
@@ -1,11 +1,27 @@
+const drawRotatedImage = (image, x, y, degrees) => {
+    context.save();
+    context.translate(x, y);
+    context.rotate(degrees * Math.PI / 180);
+    drawPic(image, -image.width / 2, -image.height / 2);
+    context.restore();
+    ANGLE_RES = degrees;
+}
+const loadImg = (path, width, height) => {
+    const img = new Image(width, height);
+    img.src = 'img/' + path;
+    return img;
+}
+const loadManyImg = (pathArr, w, h) => pathArr.map(path => loadImg(path, w, h));
+const getAudioPath = pathArr => pathArr.map(path => 'audio/' + path);
+const getRandom = (min, max) => parseInt(Math.random() * (max - min) + min);
+const drawPic = (pic, x, y) => context.drawImage(pic, x, y, pic.width, pic.height);
+
 const canvas = document.getElementById("canvas");
 const context = canvas.getContext("2d");
 
 const h = document.documentElement.clientHeight;
 const w = document.documentElement.clientWidth;
-
 const DIAMETER = w < h ? w - 50 : h - 50;
-
 canvas.height = DIAMETER;
 canvas.width = DIAMETER;
 
@@ -37,13 +53,11 @@ const mortyImg = loadManyImg(["morty1.png", "morty2.png", "morty3.png"], DIAMETE
 const assImg = loadManyImg(["ass1.png", "ass2.png", "ass3.png"], DIAMETER * 3 / 5, DIAMETER * 3 / 5);
 const cucImg = loadManyImg(["cuc1.jpg", "cuc2.png", "cuc3.png"], DIAMETER * 3 / 5, DIAMETER * 3 / 5);
 
-const wheelAudio = loadAudio("wheel.mp3");
-// let audioPerson = wheelAudio;
-const rickAudio = loadManyAudio(["rick1.mp3", "rick2.mp3", "rick3.mp3", "rick4.mp3", "rick5.mp3", "rick6.mp3"]);
-const mortyAudio = loadManyAudio(["morty1.mp3", "morty2.mp3"]);
-const assAudio = loadManyAudio(["ass1.mp3"]);
-const cucAudio = loadManyAudio(["cuc1.mp3"]);
-
+const wheelAudio = ("audio/wheel.mp3");
+const rickAudio = getAudioPath(["rick1.mp3", "rick2.mp3", "rick3.mp3", "rick4.mp3", "rick5.mp3", "rick6.mp3"]);
+const mortyAudio = getAudioPath(["morty1.mp3", "morty2.mp3"]);
+const assAudio = getAudioPath(["ass1.mp3"]);
+const cucAudio = getAudioPath(["cuc1.mp3"]);
 let soundTrack = new Audio();
 
 mainPic.onload = () => {
@@ -55,10 +69,7 @@ mainPic.onload = () => {
                 drawPic(bigFrame, 0, 0);
                 drawPic(text, 0, y);
             };
-            const timeouts = [];
-            for (let i = 0; i < yArr.length; i++) {
-                timeouts[i] = setTimeout(draw.bind(this, yArr[i]), i * 100 + 100);
-            }
+            const timeouts = yArr.map((y, i) => setTimeout(draw.bind(this, y), (i + 1) * 100));
 
             document.getElementById('canvas').insertAdjacentHTML(
                 'afterend',
@@ -73,65 +84,11 @@ mainPic.onload = () => {
     };
 };
 
-function loadAudio(path) {
-    // const audio = new Audio();
-    // audio.src = 'audio/' + path;
-    return 'audio/' + path;
-}
-
-function loadManyAudio(pathArr) {
-    const arr = [];
-    for (let i = 0; i < pathArr.length; i++) {
-        arr.push(loadAudio(pathArr[i]));
-    }
-    return arr;
-}
-
-function loadImg(path, w, h) {
-    const img = new Image();
-    img.width = w;
-    img.height = h;
-    img.src = 'img/' + path;
-    return img;
-}
-
-function loadManyImg(pathArr, w, h) {
-    const arr = [];
-    for (let i = 0; i < pathArr.length; i++) {
-        arr.push(loadImg(pathArr[i], w, h));
-    }
-    return arr;
-}
-
-function drawRotatedImage(image, x, y, degrees) {
-    context.save();
-    context.translate(x, y);
-    context.rotate(degrees * Math.PI / 180);
-    drawPic(image, -(image.width / 2), -(image.height / 2));
-    ANGLE_RES = degrees;
-    context.restore();
-}
-
-function getRandom(min, max) { // return [min, max) int
-    return parseInt(Math.random() * (max - min) + min);
-}
-
-function drawPic(pic, x, y) {
-    context.drawImage(pic, x, y, pic.width, pic.height);
-}
-
 function run(timeouts = []) {
-    for (let i = 0; i < timeouts.length; i++) {
-        clearTimeout(timeouts[i]);
-    }
+    timeouts.forEach(timeoutID => clearTimeout(timeoutID))
     context.clearRect(0, 0, canvas.width, canvas.height);
-    soundTrack.pause();
-    soundTrack.currentTime = 0.0;
     soundTrack.src = wheelAudio;
     soundTrack.play();
-    //audioPerson.pause();
-    //audioPerson.currentTime = 0.0;
-    //wheelAudio.play();
     document.getElementById('btn').disabled = true;
 
     const times = getRandom(MIN_TIMES_SCROLL, MAX_TIMES_SCROLL);
@@ -146,13 +103,9 @@ function run(timeouts = []) {
 }
 
 function getResults() {
-
-    // wheelAudio.pause();
-    // wheelAudio.currentTime = 0.0;
     ANGLE_RES = ANGLE_RES % 360;
     console.log('angle: ', ANGLE_RES);
     let img;
-
     let audioPerson;
 
     if (ANGLE_RES < 90) {
@@ -173,8 +126,6 @@ function getResults() {
         img = mortyImg[getRandom(0, mortyImg.length)];
     }
 
-    soundTrack.pause();
-    soundTrack.currentTime = 0.0;
     soundTrack.src = audioPerson;
     soundTrack.play();
 
